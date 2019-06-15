f9eb77fb0cdbb8ec31fd49d868fa6bfd41c0f72c
n1k024 <nickmarsic24@gmail.com>
2019-06-15 10:53:42 -0400

    Shuffled array in directional search to improve overall runtimes. Fixed error with converting scalar point to 2D point. The action is cast as an integer

--- Environment/Environments.py
+++ Environment/Environments.py
@@ -26,8 +26,16 @@ class Twoplayerenv(ABC):
             ## reset the environment to inital state
             self.reset_env()
 
+            #### print state
+
+            print(self.state)
+
             ## Let's a play game !!!
             self.playepisode(self.player1,self.player2)
+
+        print("Player1's Record",player1.record)
+
+        print("Player2's record",player2.record)
 
 
     @abstractmethod
@@ -39,9 +47,12 @@ class Twoplayerenv(ABC):
         if self.player1.name == "QPlayer":
 
             self.player1.state = self.state
+            self.player1.new_state = None
+
         if self.player2.state == "QPlayer":
 
             self.player2.state = self.state
+            self.player2.new_state = None
 
 
             ### console should display an empty respresentation of the environment at time step t for human player
@@ -75,19 +86,27 @@ class Twoplayerenv(ABC):
         ## each row is now a direction
         directions = np.array(directions).reshape(3,2)
 
+        np.random.shuffle(directions)
 
-        for direction in directions:
 
-            Y, X = self.convert_point(a=player.action,bound=bound_x)
+        for z in range(len(directions)):
+
+            Y, X = self.convert_point(a=int(player.action),bound=bound_x)
+
+            print("Action point is ",Y,X)
+
+            #print(directions[z][1])
+            #print(directions[z])
+
 
             count_piece = 0
-
-            if Y + direction[0] < bound_y and X + direction[1] < bound_x:
+            
+            if Y + directions[z][0] < bound_y and X + directions[z][1] < bound_x:
 
                 for x in range(depth):
 
-                    Y = Y + direction[0]
-                    X = X + direction[1]
+                    Y = Y + directions[z][0]
+                    X = X + directions[z][1]
 
 
                     #NOTE: TO SELF check if Y and X represent an actual point on the board before checking if the piece
@@ -100,31 +119,34 @@ class Twoplayerenv(ABC):
                         break
                     else:
                         count_piece = count_piece + 1
+
                     if count_piece == depth:
                         return 1
 
 
-            #### Searching in the opposite drection
-            Y, X = self.convert_point(a=player.action,bound=bound_x)
 
-            if Y - direction[0] > -1 and X - direction[1] > -1:
-                Y = Y - direction[0]
-                X = X - direction[1]
+            #### Searching in the opposite drection
+            Y, X = self.convert_point(a=int(player.action),bound=bound_x)
+
+            if Y - directions[z][0] > -1 and X - directions[z][1] > -1:
+
 
                 for x in range(depth):
-                    Y = Y - direction[0]
-                    X = X - direction[1]
+                    Y = Y - directions[z][0]
+                    X = X - directions[z][1]  
 
                     if  Y < 0 or X < 0:
                         break
 
-                    if not board[Y][X] == player.piece :
+                    if not board[Y][X] == player.piece:
                         break
                     else:
                         count_piece = count_piece + 1
 
+
                     if count_piece == depth:
                         return 1
+
 
 
         return 0
@@ -135,20 +157,28 @@ class Twoplayerenv(ABC):
         pos_x = -1
         pos_y = 0
 
+        if a == 0:
+            return 0,0
+
+
         for x in range(len(self.state)):
+
 
             pos_x = pos_x + 1
 
+            if x == a:
+                return pos_y, pos_x
 
 
-            if  x == a:
-                return pos_y,pos_x
 
 
-
-            if pos_x == bound:
+            if pos_x == bound-1:
+                print("next row !!!!")
                 pos_x = 0
                 pos_y = pos_y+1
+
+
+
 
 
 
@@ -163,6 +193,8 @@ class Twoplayerenv(ABC):
 
         if self.iswinner(player1):
 
+            print("Winner is ",player1.name)
+
             player1.record[0] += 1
             player2.record[1] += 1
 
@@ -172,36 +204,35 @@ class Twoplayerenv(ABC):
                 if player2.name == "QPlayer":
                     player2.value_update(player2.state, player2.new_state,-1)
 
-        if self.iswinner(player2):
+            elif self.iswinner(player2):
 
-            player2.record[0] +=1
-            player1.record[1] +=1
+                player2.record[0] += 1
+                player1.record[1] += 1
 
-            if player2.name == "QPlayer":
-                player2.value_update(player2.state,player2.new_state,1)
+                print("Winner is", player2.name)
+
+                if player2.name == "QPlayer":
+                    player2.value_update(player2.state,player2.new_state,1)
 
                 if player1.name == "QPlayer":
                     player1.value_update(player1.state, player1.new_state,-1)
 
 
-        if not self.iswinner(player1) and player1.name == "QPlayer":
 
-            player1.value_update(player1.state, player1.new_state,-1)
 
-        if not self.iswinner(player2) and player2.name == "QPlayer":
 
-            player2.value_update(player2.state, player2.new_state,-1)
+            elif not self.iswinner(player1) and not self.iswinner(player2):
 
-        if not self.iswinner(player1) or self.iswinner(player2):
+                print("TIE !!!")
 
-            player2.record[2] += 1
-            player1.record[2] += 1
+                player2.record[2] += 1
+                player1.record[2] += 1
 
-            if player1.name == "QPlayer":
-                player1.value_update(player1.state, player1.new_state,0)
+                if player1.name == "QPlayer":
+                    player1.value_update(player1.state, player1.new_state,0)
 
-            if player2.name == "QPlayer":
-                player2.value_update(player2.state, player2.new_state,0)
+                if player2.name == "QPlayer":
+                    player2.value_update(player2.state, player2.new_state,0)
 
     @abstractmethod
     def islegal_action(self,a):
@@ -215,49 +246,45 @@ class Twoplayerenv(ABC):
 
              while True:
 
-                    action = player1.executeaction()
-
-                    while self.illegal_action:
-
-                        if not self.islegal_action(action):
 
 
-                            action = player1.executeaction()
-                        else:
-                            self.illegal_action = False
+                    while not self.islegal_action(player1.action):
 
 
-                    self.update_env(action,player1)
+                         player1.executeaction()
+
+
+                    self.update_env(action=player1.action,player=player1)
+
+                    if player2.name == 'QPlayer':
+                        player2.set_state(player2.state)
+
+                        player2.set_next_state(self.state)
+
+                        player2.value_update(state=player2.state, new_state=player2.new_state, action=player2.action)
+
+                        player2.set_state(player2.new_state)
 
                     if self.isgameover(player=player1):
                         break
 
-                    if player2.name == 'QPlayer':
-                        player2.set_next_state(self.state)
 
-                        player2.value_update(player2.state,player2.new_state)
-
-                        player2.state = player2.new_state
                     else:
                         ## Display the current state of the environment for  our human player
                         self.display_state(self.state)
 
-                    self.illegal_action = True
 
-                    action =  player2.executeaction()
 
-                    while self.illegal_action:
 
-                        if not self.islegal_action(action):
 
-                            action = player2.executeaction()
+                    while not self.islegal_action(player2.action):
 
-                        else:
 
-                            self.illegal_action = False
+                             player2.executeaction()
+
 
                     ## update our environment after executing actions onto it
-                    self.update_env(action,player2)
+                    self.update_env(player2.action,player2)
 
                 ## if the game is over we get out of the loop
                     if self.isgameover(player=player2):
@@ -268,7 +295,7 @@ class Twoplayerenv(ABC):
 
                         player1.set_next_state(self.state)
 
-                        player1.value_update(player1.state, player1.new_state)
+                        player1.value_update(player1.state, player1.new_state,action=player1.action)
 
                         player1.state = player1.new_state
                     else:
@@ -286,12 +313,20 @@ class Twoplayerenv(ABC):
 class TicTacToe(Twoplayerenv):
     def __init__(self,player1,player2):
 
+        self.player1 = player1
+
+        self.player2 = player2
+
         self.player1.piece = "X"
 
         self.player2.piece = "O"
 
 
+
         Twoplayerenv.__init__(self, player1=player1,player2 = player2)
+
+
+
 
 
     def reset_env(self):
@@ -310,11 +345,19 @@ class TicTacToe(Twoplayerenv):
 
         switch = True
 
-        if  a < 0 and  a > 9 or not self.state[a] == "-":
-            switch = False
+
+        a = int(a)
+
+        print("Action = ",a)
+
+        if  a < 0 or  a > 8:
+            return False
+
+        if not self.state[a]== "-":
+            return  False
 
 
-            return switch
+        return switch
 
 
     def isgameover(self,player):
@@ -330,6 +373,7 @@ class TicTacToe(Twoplayerenv):
         ## Terminate this function  if we determine the inputted is the winner as a result of their last action
 
         if winner:
+            print("Terminating due to winner")
             return winner
 
 
@@ -362,6 +406,9 @@ class TicTacToe(Twoplayerenv):
     def update_env(self, action,player):
 
         ## Player applies an action using their game onto the environment
+
+        action = int(action)
+
         self.state[action] = player.piece
 
     def iswinner(self,player):
@@ -371,7 +418,10 @@ class TicTacToe(Twoplayerenv):
         board = np.array(self.state)
 
         board = board.reshape(3,3)
-        return self.directional_search(board=board,player=player,bound_x=board.shape[0],bound_y=board.shape[1],depth=2)
+
+        print("Winner ?????",self.directional_search(board=board,player=player,bound_x=board.shape[1],bound_y=board.shape[0],depth=2))
+
+        return self.directional_search(board=board,player=player,bound_x=board.shape[1],bound_y=board.shape[0],depth=2)
 
 
 
