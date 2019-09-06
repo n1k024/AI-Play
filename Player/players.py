72425611b3bffcb96bb89db387849fa636922f50
n1k024 <nickmarsic24@gmail.com>
2019-09-06 17:02:37 -0400

    Created a new N-stepQ-Learning agent which performs N-step bootstrap updates given the parameter N. This is a variant of the popular Q-Learning

--- Player/players.py
+++ Player/players.py
@@ -22,7 +22,7 @@ class Player(ABC):
 
 
 class TabularRLAgent(ABC):
-    def __init__(self, alpha=.1,gamma = .9):
+    def __init__(self, alpha=.1, gamma=.9):
         self.values = collections.defaultdict(float)
         self.state = ""
         self.new_state = ""
@@ -31,7 +31,7 @@ class TabularRLAgent(ABC):
         self.GAMMA = gamma
 
     @abstractmethod
-    def value_update(self, state, action, new_state, reward):
+    def value_update(self, state, action, new_state, reward, done=0):
         pass
 
     def set_next_state(self, next_state):
@@ -77,11 +77,11 @@ class HumanPlayer(Player):
 
 
 class QPlayer(Player, TabularRLAgent):
-    def __init__(self, alpha=.01, name="QPlayer",gamma = .9):
+    def __init__(self, alpha=.01, name="QPlayer", gamma=.9):
 
         Player.__init__(self, name=name)
 
-        TabularRLAgent.__init__(self, alpha=alpha,gamma = gamma)
+        TabularRLAgent.__init__(self, alpha=alpha, gamma=gamma)
 
     def bestactionandvalue(self, state):
         switch = 1
@@ -96,7 +96,7 @@ class QPlayer(Player, TabularRLAgent):
             if best_value is None or best_value < action_value:
                 best_value = action_value
                 best_action = action
-            if best_value != action_value :
+            if best_value != action_value:
                 switch = 0
 
         if best_value == 0:
@@ -107,13 +107,13 @@ class QPlayer(Player, TabularRLAgent):
 
         return best_value, best_action
 
-    def value_update(self, state, action, new_state, reward=0):
+    def value_update(self, state, action, new_state, reward=0, done=0):
 
         prev_val = self.values[(state, action)]
 
         _, action_val = self.bestactionandvalue(new_state)
 
-        self.values[(state, action)] = prev_val + self.ALPHA * (reward +  (self.GAMMA*action_val - prev_val))
+        self.values[(state, action)] = prev_val + self.ALPHA * (reward + (self.GAMMA * action_val - prev_val))
 
     def executeaction(self):
         ## compute best action in a given state
@@ -129,68 +129,65 @@ class QPlayer(Player, TabularRLAgent):
 ### This RL agent uses a technique known as Double Learning ,it can be applied to almost any TD method such as SARSA and Q-Learning
 ## This Agent will use double learning to avoid maximization bias obtained from the max operation in Q-Learning
 class DoubleQPLayer(TabularRLAgent, Player):
-    def __init__(self, alpha=.1, name="DbQPlayer",gamma=.9):
+    def __init__(self, alpha=.1, name="DbQPlayer", gamma=.9):
         self.v2 = collections.defaultdict(float)
 
-        Player.__init__(self,name=name)
+        Player.__init__(self, name=name)
 
-        TabularRLAgent.__init__(self,alpha=alpha,gamma = gamma)
+        TabularRLAgent.__init__(self, alpha=alpha, gamma=gamma)
 
-    def value_update(self,state,action,new_state,reward=0):
+    def value_update(self, state, action, new_state, reward=0, done=0):
 
-        Z = random.uniform(0,1)
+        Z = random.uniform(0, 1)
 
-    ### Updating of Q-values occurs here notice we are using two lookup table here, we basically flip a coin to determine which table we will update
+        ### Updating of Q-values occurs here notice we are using two lookup table here, we basically flip a coin to determine which table we will update
 
         if Z > .5:
-            action =  self.best_action(self.values,new_state)
+            action = self.best_action(self.values, new_state)
 
-            self.values[(state,action)] = self.values[(state,action)] + self.ALPHA   \
-                                          *(reward + (self.GAMMA *self.v2[(state,action)]  - self.values[(state,action)]))
+            self.values[(state, action)] = self.values[(state, action)] + self.ALPHA \
+                                           * (reward + (
+                    self.GAMMA * self.v2[(state, action)] - self.values[(state, action)]))
         elif Z < .5:
 
-            action = self.best_action(self.v2,new_state)
+            action = self.best_action(self.v2, new_state)
 
             self.v2[(state, action)] = self.v2[(state, action)] + self.ALPHA \
-                                       *( reward + (self.GAMMA * self.values[(state, action)] - self.v2[(state, action)]))
+                                       * (reward + (
+                    self.GAMMA * self.values[(state, action)] - self.v2[(state, action)]))
         else:
-            self.value_update(state,action,new_state,reward)
+            self.value_update(state, action, new_state, reward)
 
+    def best_action(self, values, state):
+        best_value = -1
+        best_action = 0
+        switch = 1
 
+        for action in range(10):
+            if values[(state, action)] > best_value:
+                best_value = values[(state, action)]
 
+                best_action = action
 
-    def best_action(self,values,state):
-         best_value = -1
-         best_action = 0
-         switch = 1
+        if best_value != 0:
+            switch = 0
+        elif best_value == 0:
+            switch = 1
 
-         for action in range(10):
-             if values[(state, action)] > best_value:
+        if switch:
+            best_action = random.uniform(0, 10)
 
-                 best_value = values[(state, action)]
-
-                 best_action = action
-
-         if best_value != 0:
-             switch = 0
-         elif best_value == 0:
-             switch = 1
-
-         if switch:
-             best_action= random.uniform(0,10)
-
-         return best_action
-
+        return best_action
 
     def executeaction(self):
 
-    #### Here half the time we will execute actions using one looks up table and the rest of the time we will use the other table for our table to make its choice of action
+        #### Here half the time we will execute actions using one looks up table and the rest of the time we will use the other table for our table to make its choice of action
 
-        Z = random.uniform(0,1)
+        Z = random.uniform(0, 1)
         if Z > .5:
-            self.action = self.best_action(self.values,self.state)
+            self.action = self.best_action(self.values, self.state)
         elif Z < .5:
-            self.action = self.best_action(self.v2,self.state)
+            self.action = self.best_action(self.v2, self.state)
         else:
             ## If there are ties at which it is .5 we call the executeaction again
             self.executeaction()
@@ -198,5 +195,59 @@ class DoubleQPLayer(TabularRLAgent, Player):
         return self.action
 
 
+class NStepQAgent(Player, TabularRLAgent):
+    def __init__(self, N, name, gamma=.9, alpha=.1):
 
+        #### N step Boostrapping agent that performs Q-Learning updates using partial returns consisting of N steps
 
+        self.N = N
+
+        self.r = []
+
+        self.actions = []
+
+        self.states = []
+
+        Player.__init__(self, name=name)
+        TabularRLAgent.__init__(self, alpha=alpha, gamma=gamma)
+
+    def value_update(self, state, action, new_state, reward, done=0):
+
+        if not self.r:
+            self.r.append(reward)
+            self.actions.append(action)
+            self.states.append(state)
+
+        ## Store the trasition (s',a,r) in lists
+        self.r.append(reward)
+        self.states.append(new_state)
+        self.actions.append(action)
+
+        if len(self.r) == self.N + 1 or done:
+
+            Gt = 0
+
+            for x in range(self.N):
+                br = self.r.pop()
+                ba = self.actions.pop()
+                bs = self.states.pop()
+
+                ### Compute the N-step return sum of future discounted reward
+                Gt = br + (self.GAMMA ** x) * Gt
+
+            s = self.states.pop()
+            a = self.actions.pop()
+
+            ####### Update our lookup table of Q-values using the N-step return that we just computed (Gt)
+            self.values[(s, a)] = self.values[(s, a)] + self.ALPHA * (Gt - self.values[(s, a)])
+
+    def executeaction(self):
+
+        ### Semi-Greedy Action Selection
+        action = self.best_action(self.state)
+
+        #### Epsilon Greedy Action Selection
+
+        self.action = action
+
+        return action
