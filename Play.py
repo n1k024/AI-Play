3e65998f9e2ae7c16a1eb2acbff0c31ec1fb0bcb
n1k024 <nickmarsic24@gmail.com>
2019-11-19 08:33:39 +0100

    Added Commandline Arguments to allow users to decide how many games or episodes they wish to play

--- Play.py
+++ Play.py
@@ -2,8 +2,19 @@ from Player import players
 
 from Environment import Environments
 
-#### Player objects created here
+import argparse
 
+### Command line arugments
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--games', type=int, help="Number of games you wish to play this environment", default=1)
+
+args = parser.parse_args()
+
+games = args.games
+
+
+#### Player objects created here
 
 ## This is a human player
 hp = players.HumanPlayer()
@@ -16,4 +27,4 @@ ndqp = players.NStepDoubleQAgent()
 
 ### Creating an enviornment object with HumanPlayer and N-stepDouble Q-Player they will play 20 episodes or games together of the
 #### Tic Tac Toe Evironment
-Environments.TicTacToe(player1=hp, player2=qp, episodes=20)
+Environments.TicTacToe(player1=hp, player2=qp, episodes=games)
