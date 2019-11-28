ab38aafe6317067c065dcf0b783ee9f7ce0bdb9b
n1k024 <nickmarsic24@gmail.com>
2019-11-28 08:37:53 +0100

    Fixes to agents when termination of episode occurs. Now upon termination all actions of the agent will be set to None

--- Player/players.py
+++ Player/players.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0ffac184ac83794860a8712de8fbb17f459c48f0e58918c90fa9be61ac4db385
-size 23370
+oid sha256:d3e665cc3e9ffac19c61f180debd55675b7a29853b006f8bb583ebb12e9f8a52
+size 23665
