fbd150818b83ca6db3b95e28493a996c2fb46b95
n1k024 <nickmarsic24@gmail.com>
2020-01-12 08:33:36 -0500

    LFS is turned off for Python scripts this was interfering with how our source code looked when the repository was cloned

--- Play.py
+++ Play.py
@@ -1,3 +1,33 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7fb00dadb07549f4001cff11c015d904f5cd0e0e2bf93bb952918ac0c6e23f58
-size 750
+from Player import players
+
+from Environment import Environments
+
+import argparse
+
+### Command line arugments
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--games', type=int, help="Number of games you wish to play this environment", default=1)
+
+args = parser.parse_args()
+
+games = args.games
+
+#### Player objects created here
+
+## This is a human player
+hp = players.HumanPlayer()
+
+qp = players.QPlayer()
+
+dqp = players.DoubleQPLayer()
+
+ndqp = players.NStepDoubleQAgent()
+
+sarsa = players.SARSAgent()
+
+### Creating an enviornment object with HumanPlayer and N-stepDouble Q-Player they will play 20 episodes or games together of the
+#### Tic Tac Toe Evironment
+#### Player
+
+Environments.TicTacToe(player1=sarsa, player2=hp, episodes=games)
